9b90bb80673c7131b80a4fcaf245e25af3b707e6
7jcdev <7jaydev@protonmail.com>
2024-12-11 17:49:53 -0600

    Foundation

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), mdx(), sitemap()],
+  markdown: {
+    shikiConfig: {
+      theme: 'material-theme-darker',
+    },
+  },
+  site: "https://CyberThrob.github.io",
+});
